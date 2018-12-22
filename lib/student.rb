26732dc4e81bb72ead001b8b58e206fb8ac23d12
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 05:22:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,10 +76,11 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
+      LIMIT ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
